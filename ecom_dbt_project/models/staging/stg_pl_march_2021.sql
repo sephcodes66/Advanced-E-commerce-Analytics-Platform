466c4b70fb4965f2c271c38d56ad3916389c4084
sephcodes66 <m09758610@gmail.com>
2025-07-21 22:47:08 +0200

    feat: Clean and refresh project structure

--- ecom_dbt_project/models/staging/stg_pl_march_2021.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    TRIM(LOWER(Sku)) AS product_sku,
-    TRIM(LOWER(Category)) AS product_category,
-    TRY_CAST("TP 1" AS DECIMAL(10, 2)) AS cost_price_tp1,
-    TRY_CAST("TP 2" AS DECIMAL(10, 2)) AS cost_price_tp2,
-    TRY_CAST("Final MRP Old" AS DECIMAL(10, 2)) AS final_mrp_old,
-    'pl_march_2021' AS _source_file_name
-FROM read_csv_auto('/Users/shabhrishreddyuddehal/Downloads/dbt/data/raw/P  L March 2021.csv')
-WHERE Sku IS NOT NULL
